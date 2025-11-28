9ace4afcdd0e9218a79f29b92f45f5a21543332f
hanhafizh <farhanhafizh770@gmail.com>
2025-11-28 15:43:51 +0700

    add todolist migration, seeders, controllers.. add index todo and custom response in resource

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TodolistResource;
+use App\Models\Todolist;
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $todolist = Todolist::latest()->get();
+
+        return  TodolistResource::collection($todolist);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\TodolistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,7 +12,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 /* Auth */
-
 Route::post('/register', RegisterController::class);
 Route::post('/login', LoginController::class);
 Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+
+/* Todos */
+Route::apiResource('/todos', TodolistController::class)->middleware('auth:sanctum');
