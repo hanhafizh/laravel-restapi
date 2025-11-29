5f36e3cbf1d06cb854db8079883cc3ef1b849baf
hanhafizh <farhanhafizh770@gmail.com>
2025-11-29 14:05:12 +0700

    custom logging database api

--- app/Http/Controllers/TodolistController.php
+++ app/Http/Controllers/TodolistController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\TodolistResource;
+use App\Models\Logging;
 use App\Models\Todolist;
-use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+
+use Exception;
+
 
 class TodolistController extends Controller
 {
@@ -14,9 +19,17 @@ class TodolistController extends Controller
      */
     public function index()
     {
-        $todolist = Todolist::latest()->get();
+        try {
+            $todolist = Todolist::latest()->get();
 
-        return  TodolistResource::collection($todolist);
+            return  TodolistResource::collection($todolist);
+        } catch (Exception $error) {
+            Log::error("Todolist failed " . $error->getMessage());
+
+            Logging::record(Auth::user()->id, 'Todolist failed ' . $error->getMessage());
+
+            return response()->json(['message' => 'Todolist failed ' . $error->getMessage()], 500);
+        }
     }
 
     /**
@@ -38,7 +51,9 @@ class TodolistController extends Controller
                 'data' => new TodolistResource($todolist)
             ], 201);
         } catch (Exception $error) {
-            return response()->json(['message' => 'Todolist created failed'], 500);
+            Logging::record(Auth::user()->id, 'Todolist created failed ' . $error->getMessage());
+
+            return response()->json(['message' => 'Todolist created failed' . $error->getMessage()], 500);
         }
     }
 
@@ -50,7 +65,9 @@ class TodolistController extends Controller
         $todolist = Todolist::find($id);
 
         if ($todolist == null) {
-            return response()->json(['message' => 'Todolist not found'], 404);
+            Logging::record(Auth::user()->id, 'Todolist not found ' . $id);
+
+            return response()->json(['message' => 'Todolist not found ' . $id], 404);
         }
 
         return new TodolistResource($todolist);
@@ -70,7 +87,9 @@ class TodolistController extends Controller
         $todolist = Todolist::find($id);
 
         if ($todolist == null) {
-            return response()->json(['message' => 'Todolist not found'], 404);
+            Logging::record(Auth::user()->id, 'Todolist not found ' . $id);
+
+            return response()->json(['message' => 'Todolist not found ' . $id], 404);
         }
 
         try {
@@ -81,7 +100,9 @@ class TodolistController extends Controller
                 'data' => new TodolistResource($todolist)
             ], 200);
         } catch (Exception $error) {
-            return response()->json(['message' => 'Todolist updated failed'], 500);
+            Logging::record(Auth::user()->id, 'Todolist updated failed ' . $error->getMessage());
+
+            return response()->json(['message' => 'Todolist updated failed' . $error->getMessage()], 500);
         }
     }
 
@@ -93,17 +114,20 @@ class TodolistController extends Controller
         $todolist = Todolist::find($id);
 
         if ($todolist == null) {
-            return response()->json(['message' => 'Todolist not found'], 404);
+            Logging::record(Auth::user()->id, 'Todolist not found ' . $id);
+
+            return response()->json(['message' => 'Todolist not found ' . $id], 404);
         }
 
         try {
             $todolist->delete();
 
             return response()->json([
-                'message' => 'Todolist deleted sucessfully',
+                'message' => 'Todolist deleted sucessfully id: ' . $id,
             ], 200);
         } catch (Exception $error) {
-            return response()->json(['message' => 'Todolist deleted failed'], 500);
+            Logging::record(Auth::user()->id, 'Todolist deleted failed ' . $error->getMessage());
+            return response()->json(['message' => 'Todolist deleted failed' . $error->getMessage()], 500);
         }
     }
 }
---
+++ database/migrations/2025_11_28_132025_create_loggings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loggings', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id')->nullable(); // 1, 2
+            $table->string('ip_address'); // 192.168.0.1
+            $table->string('message'); // Pesan
+            $table->string('action'); // GET, POST, PUT, PATCH, DELETE
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loggings');
+    }
+};
